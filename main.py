066e52b1f4af847e07d79612370d3eb6a4894661
xrizer <aqsholafifi@gmail.com>
2022-06-13 08:47:08 +0700

    final

---
+++ main.py
@@ -0,0 +1,25 @@
+from testing import *
+from flask import Flask, jsonify, render_template,request,send_file,Response
+from flask_cors import CORS
+import os
+
+model1, model2, model3 = load_models()
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/',methods=['GET'])
+def hello_world():
+    return jsonify({'test':'berhasil'})
+
+
+@app.route('/predict',methods=['POST'])
+def coba():
+    imagefile = request.files['file']
+    img_path = 'images/' + imagefile.filename
+    imagefile.save(img_path)
+    result, peluang, count = main(img_path, model1, model2, model3)
+    hasil = report(result, peluang, count)
+    return jsonify({'diagnose': hasil})
+
+if __name__ == '__main__':
+    app.run()
